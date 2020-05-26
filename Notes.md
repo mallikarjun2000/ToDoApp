feec68ad7f2efc05f3322047d685d9e8cb139a28
mallikarjun2000 <arjunmallik093@gmail.com>
2020-05-26 22:42:25 +0530

    User Authentication added

---
+++ Notes.md
@@ -0,0 +1,5 @@
+ Login - ok
+ Register - ok
+ todo Added - ok
+ todo modify - ok
+ token - ok
--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userLogin = require('../models/usersLogin');
 const jwt = require('jsonwebtoken');
-const dorenv = require('dotenv')
+const dorenv = require('dotenv');
+const userDetails = require('../models/userDetailsSchema');
 
 dorenv.config();
 
@@ -13,9 +14,9 @@ router.get('/',async (req,res)=>{
 router.post('/',async (req,res)=>{
     const user = await userLogin.findOne({email:req.body.email});
     if(!user) res.json({message:"user doesn't exist"});
-
-    const token = jwt.sign({_id: userLogin._id},process.env.TOKEN_SECRET)
-    res.header('auth-token',token).send(token);
+    const userDetail = await userDetails.findOne({email: req.body.email});
+    const token = jwt.sign({_id: userLogin._id},process.env.TOKEN_SECRET);
+    res.header('auth-token',token).json({message:'logined Succesfully',body:userDetail._id});
 })
 
 module.exports = router;
