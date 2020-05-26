58cf3ea7f2cf0a3aac1468ef36b4343eb5c4dbf2
mallikarjun2000 <arjunmallik093@gmail.com>
2020-05-26 20:49:51 +0530

    Toekn under testing

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const userLogin = require('../models/usersLogin');
+const jwt = require('jsonwebtoken');
+const dorenv = require('dotenv')
+
+dorenv.config();
 
 router.get('/',async (req,res)=>{
+    console.log('Token secret is ',process.env.TOKEN_SECRET);
 })
 
 router.post('/',async (req,res)=>{
+    const user = await userLogin.findOne({email:req.body.email});
+    if(!user) res.json({message:"user doesn't exist"});
+
+    const token = jwt.sign({_id: user._id},process.env.TOKEN_SECRET)
+
+
 })
 
 module.exports = router;
